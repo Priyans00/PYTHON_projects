b78c0f6501c6048fd8de49225fe2889898c11ddc
Priyans00 <115811175+Priyans00@users.noreply.github.com>
2024-11-26 20:57:53 +0530

    Initial commit

---
+++ 21_game/python.py
@@ -0,0 +1,69 @@
+import random 
+def main():
+    flag = True
+    k = int(input("you want to start first(1) or second(2)"))
+    global n
+    n = []
+    while flag == True:
+        if k == 1:
+            human(n)
+            if check(n)==False:
+                break
+            comp(n)
+            if check(n)==False:
+                break
+        if k == 2:
+            comp(n)
+            if check(n)==False:
+                break
+            human(n)
+            if check(n)==False:
+                break
+        flag=check(n)
+
+def check(n):
+    if 21 in n:
+        return False
+    else:
+        return True
+
+def human(n):
+    if n == []:
+        l = 1
+    else:
+        l = n[-1]+1
+    k = int(input("how many no you want to enter"))
+    if k not in [1,2,3]:
+        print('you are disqualified from the game ')
+        n.append(21)
+    for i in range(l,l+k):
+        n.append(i)
+    if 21 in n:
+        loose('you')
+    else :
+        print("order of input after your turn is :")
+        print(n)
+
+def comp(n):
+    if n == []:
+        l = 1
+    else:
+        l = n[-1]+1
+    k=random.randint(1,3)
+    for i in range(l,l+k):
+        n.append(i)
+    if 21 in n:
+        loose('comp')
+    print("order of input after comp's turn is :")
+    print(n)
+
+def loose(s):
+    print(f"{s} lost")
+
+def start():
+    k = input("you want to play ? (y/Y)")
+    while k == 'y' or k == 'Y':
+        n=[]
+        main()
+        k = input('you want to play again ? (y/Y)')
+start()
