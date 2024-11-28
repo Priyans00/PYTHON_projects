478f57235e3093dffeebbb520e5c796b90e09058
Priyans00 <115811175+Priyans00@users.noreply.github.com>
2024-11-28 21:21:58 +0530

    used pysricpt

--- BEGINNER_PROJECTS/FLAMES game/python.py
+++ BEGINNER_PROJECTS/FLAMES game/python.py
@@ -1,16 +1,31 @@
-def main():
-    n1=input("enter 1st name")
-    n2=input("enter 2nd name")
-    l1=list(n1)
-    l2=list(n2)
-    temp=l1.copy()
+from js import document
+
+def main(*args):
+    # Add error checking for elements
+    name1_elem = document.getElementById('name1')
+    name2_elem = document.getElementById('name2')
+    
+    if not name1_elem or not name2_elem:
+        print("Error: Input elements not found!")
+        return
+    
+    n1 = name1_elem.value
+    n2 = name2_elem.value
+    
+    if not n1 or not n2:
+        document.getElementById('result').innerHTML = "Please enter both names"
+        return
+        
+    l1 = list(n1)
+    l2 = list(n2)
+    temp = l1.copy()
     for i in temp:
         if i in l2:
             l1.remove(i)
             l2.remove(i)
-    k=len(l1)+len(l2)
-    l=flames(k)
-    rel_check(l)
+    k = len(l1) + len(l2)
+    l = flames(k)
+    return l
 
 def flames(n):
     l=['F','L','A','M','E','S'] 
@@ -37,16 +52,33 @@ def flames(n):
     return l
 
 def rel_check(l):
-    
+    if not l:
+        return "Error in calculation"
+        
     relationships = {
-    'F': 'Friends',
-    'L': 'Lovers',
-    'A': 'Affection',
-    'M': 'Marriage',
-    'E': 'Enemies',
-    'S': 'Siblings'
+        'F': 'Friends',
+        'L': 'Lovers',
+        'A': 'Affection',
+        'M': 'Marriage',
+        'E': 'Enemies',
+        'S': 'Siblings'
     }
-    k=relationships[l[0]]
-    print(f"The relationship status is: {k}")
+    k = relationships[l[0]]
+    result_elem = document.getElementById('result')
+    if result_elem:
+        result_elem.innerHTML = k
 
-main()
+def calculate_flames(*args):
+    l = main()
+    if l:
+        rel_check(l)
+
+# Wait for document to load before binding
+def init():
+    button = document.getElementById('calculate')
+    if button:
+        button.onclick = calculate_flames
+
+# Call init when document is ready
+init()
+
