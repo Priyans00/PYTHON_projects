f1f307ee60740d9cc9454940de83ccdd06a39ff5
Priyans00 <115811175+Priyans00@users.noreply.github.com>
2024-12-15 22:28:10 +0530

    added to_do_list

---
+++ TKINTER/app.py
@@ -0,0 +1,107 @@
+from tkinter import *
+from tkinter import messagebox 
+import sqlite3
+
+conn = sqlite3.connect("app_data.db")
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS data(
+               id INTEGER PRIMARY KEY AUTOINCREMENT,
+               key TEXT,
+               value TEXT)
+""")
+conn.commit()
+
+root = Tk()
+
+root.title('TO DO LIST')
+root.geometry("420x500")
+root.configure(bg = "lightgrey")
+
+
+a=root.winfo_width()
+b=root.winfo_height()
+
+
+f1 = Frame(root,bg='black',height=400,width=400)
+f1.place(anchor="center",relx=0.5,rely=0.65)
+
+f2 = Frame(root,bg='black',width=300,height=70)
+f2.place(anchor='center',relx=0.5,rely=0.15)
+
+l2=Label(f2,text="TO DO LIST",bg='black',fg='yellow')
+l2.place(anchor='center',relx=0.5,rely=0.5)
+
+
+
+def insert1():
+    insert = Tk()
+    insert.title("Add Task")
+    insert.geometry("300x200")
+
+    fi1 = Frame(insert)
+    fi1.place(anchor='center',relx=0.5,rely=0.5)
+
+    Label(fi1, text="Enter the task:").pack()
+    ei1 = Entry(fi1)
+    ei1.pack()
+    
+    Label(fi1, text="Enter the deadline:").pack()
+    ei2 = Entry(fi1)
+    ei2.pack()
+
+    def add():
+        key = ei1.get()
+        value = ei2.get()
+
+        if key and value:
+            try:
+                cursor.execute("insert into data (key,value) values (?,?)",(key,value))
+                conn.commit()
+                messagebox.showinfo("Success","Data saved successfully!")
+                insert.destroy()
+            except Exception as e:
+                messagebox.showerror("Error",f"An error occurred: {str(e)}")
+        else:
+            messagebox.showwarning("Warning","Please fill in both fields")
+
+    bi1 = Button(fi1, text="Submit", command=add)
+    bi1.pack(pady=10)
+
+    insert.mainloop()
+
+def show():
+    show = Toplevel(root)
+
+    fs1 = Frame(show)
+    fs1.place(anchor = 'center',relx=0.5,rely=0.5)
+
+    cursor.execute('select * from data')
+    result = cursor.fetchall()
+    conn.commit()
+
+    k=len(result)
+    newh = max(100,k*20)
+    show.geometry(f"500x{newh}")
+    for i in range(len(result)):
+        label = Label(fs1, text=f"s.no = {result[i][0]}, task = {result[i][1]}, deadline = {result[i][2]}")
+        label.grid(row=i, column=0)
+
+    def dest():
+        show.destroy()
+
+    b1 = Button(fs1, text="return to main menu", command=dest)
+    b1.grid(row=5, column=10)
+    show.mainloop()
+    
+
+b1 = Button(f1,text = "enter a task",height=2,width=15,bg="blue",fg="yellow",padx=5,borderwidth=0.5,command=insert1)
+b1.place(anchor='w',relx=0,rely=0.25)
+
+b2 = Button(f1,text="see the tasks",height=2,width=15,bg="blue",fg="yellow",padx=5,borderwidth=5,command=show)
+b2.place(anchor='w',relx=0,rely=0.5)    
+
+
+
+root.mainloop()
