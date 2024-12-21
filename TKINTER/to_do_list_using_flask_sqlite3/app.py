1638cd8a26c7bf4d3c9fa6a728196a5b0d6007fc
Priyans00 <115811175+Priyans00@users.noreply.github.com>
2024-12-21 09:26:14 +0530

    fix

--- TKINTER/to_do_list_using_flask_sqlite3/app.py
+++ TKINTER/to_do_list_using_flask_sqlite3/app.py
@@ -1,8 +1,11 @@
 from tkinter import *
 from tkinter import messagebox 
 import sqlite3
+import os
 
-conn = sqlite3.connect("app_data.db")
+
+db_path = os.path.join(os.path.dirname(__file__),"app_data.db")
+conn = sqlite3.connect(db_path)
 cursor = conn.cursor()
 
 cursor.execute("""
